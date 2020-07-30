de361f6b3bc6486e7a59de9eed2eb7574f7cd151
Aakash Shah <ashah@lowes.com>
2020-07-30 11:15:17 +0530

    lint on save

--- src/__tests__/fizz-buzz.js
+++ src/__tests__/fizz-buzz.js
@@ -1,37 +1,37 @@
 
 const fizzBuzz = (n) => {
-  for(let i = 1; i <= n; i++) {
-    const isMultipleOf3 = !(i % 3)
-    const isMultipleOf5 = !(i % 5)
-    const FIZZ = 'Fizz'
-    const BUZZ = 'Buzz'
-    if(isMultipleOf3 && isMultipleOf5) console.log(`${FIZZ}${BUZZ}`);
-    else if(isMultipleOf3) console.log(FIZZ);
-    else if(isMultipleOf5) console.log(BUZZ);
+  for (let i = 1; i <= n; i++) {
+    const isMultipleOf3 = !(i % 3);
+    const isMultipleOf5 = !(i % 5);
+    const FIZZ = 'Fizz';
+    const BUZZ = 'Buzz';
+    if (isMultipleOf3 && isMultipleOf5) console.log(`${FIZZ}${BUZZ}`);
+    else if (isMultipleOf3) console.log(FIZZ);
+    else if (isMultipleOf5) console.log(BUZZ);
     else console.log(i);
   }
-}
+};
 
-const consoleLog = console.log
+const consoleLog = console.log;
 afterEach(() => {
-  console.log = consoleLog // replace the original to avoid flaky tests
-})
+  console.log = consoleLog; // replace the original to avoid flaky tests
+});
 
-let consoleOutputs = []
-let mockedConsoleLog = (log) => consoleOutputs.push(log)
+const consoleOutputs = [];
+const mockedConsoleLog = (log) => consoleOutputs.push(log);
 beforeEach(() => {
-  console.log = mockedConsoleLog
-})
+  console.log = mockedConsoleLog;
+});
 
 describe('fizzBuzz method', () => {
   it('correctly print as per requirements', () => {
-    fizzBuzz(100)
-    const i = 1
-    expect(consoleOutputs[1 - i]).toBe(1)
-    expect(consoleOutputs[2 - i]).toBe(2)
-    expect(consoleOutputs[3 - i]).toBe('Fizz')
-    expect(consoleOutputs[4 - i]).toBe(4)
-    expect(consoleOutputs[90 - i]).toBe('FizzBuzz')
-    expect(consoleOutputs[5 - i]).toBe('Buzz')
-  })
-})
+    fizzBuzz(100);
+    const i = 1;
+    expect(consoleOutputs[1 - i]).toBe(1);
+    expect(consoleOutputs[2 - i]).toBe(2);
+    expect(consoleOutputs[3 - i]).toBe('Fizz');
+    expect(consoleOutputs[4 - i]).toBe(4);
+    expect(consoleOutputs[90 - i]).toBe('FizzBuzz');
+    expect(consoleOutputs[5 - i]).toBe('Buzz');
+  });
+});
