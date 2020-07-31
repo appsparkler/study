62aedfade727594cd58f2f716411343f51fd18c1
Aakash Shah <ashah@lowes.com>
2020-07-31 12:58:28 +0530

    lint auto-fix

--- __tests__/active-traders.js
+++ __tests__/active-traders.js
@@ -13,44 +13,44 @@
 const reduceCustomerNamesToIncludeNumberOfTrades = (
     activeList, customer, idx, arr
 ) => {
-  const customerAdded = activeList.find((c) => c.customer === customer);
+  const customerAdded = activeList.find((c) => c.customer === customer)
   if (!customerAdded) {
-    const trades = arr.filter((c) => c === customer).length;
-    const totalTrades = arr.length;
+    const trades = arr.filter((c) => c === customer).length
+    const totalTrades = arr.length
     const result = {
       trades,
       customer,
       percent: (trades/totalTrades) * 100,
-    };
-    activeList.push(result);
+    }
+    activeList.push(result)
   }
-  return activeList;
-};
+  return activeList
+}
 
 const sortByName = (a, b) => {
-  if (a < b) return -1;
-  else if (b < a) return 1;
-  else return 0;
-};
+  if (a < b) return -1
+  else if (b < a) return 1
+  else return 0
+}
 
-const sortCustomersAsPerTrade = (b = {}, a = {}) => a.percent - b.percent;
+const sortCustomersAsPerTrade = (b = {}, a = {}) => a.percent - b.percent
 
-const atLeast5PercentFilter = (c) => c.percent >= 5;
+const atLeast5PercentFilter = (c) => c.percent >= 5
 
 const mostActive = (customers = []) => customers
     .reduce(reduceCustomerNamesToIncludeNumberOfTrades, [])
     .sort(sortCustomersAsPerTrade)
     .filter(atLeast5PercentFilter)
     .map((c) => c.customer)
-    .sort(sortByName);
+    .sort(sortByName)
 
 describe('srotByName', () => {
   it('should sort customer list by name', () => {
-    const customers = ['Omega', 'Alpha'];
-    const result = customers.sort(sortByName);
-    expect(result[0]).toBe('Alpha');
-  });
-});
+    const customers = ['Omega', 'Alpha']
+    const result = customers.sort(sortByName)
+    expect(result[0]).toBe('Alpha')
+  })
+})
 
 describe('sortCustomersAsPerPercent', () => {
   it(`sort the customer list based on their trades
@@ -65,49 +65,49 @@ describe('sortCustomersAsPerPercent', () => {
         trades: 90,
         percent: 90,
       },
-    ];
-    const result = customers.sort(sortCustomersAsPerTrade);
+    ]
+    const result = customers.sort(sortCustomersAsPerTrade)
     expect(result[0]).toEqual({
       trades: 90,
       customer: 'Omega',
       percent: 90,
-    });
-  });
-});
+    })
+  })
+})
 
 describe('reduceCustomerNamesToIncludeNumberOfTrades', () => {
   it(`should reduce the list of customers to a
       list which give a total of the customers
       trades and their name.
 `, () => {
-    const customers = ['Omega', 'Alpha', 'Alpha', 'Alpha', 'Alpha'];
+    const customers = ['Omega', 'Alpha', 'Alpha', 'Alpha', 'Alpha']
     const result = customers
-        .reduce(reduceCustomerNamesToIncludeNumberOfTrades, []);
+        .reduce(reduceCustomerNamesToIncludeNumberOfTrades, [])
     expect(result[0]).toEqual({
       trades: 1,
       customer: 'Omega',
       percent: 20,
-    });
+    })
     expect(result[1]).toEqual({
       trades: 4,
       customer: 'Alpha',
       percent: 80,
-    });
-    expect(result[2]).toBe(undefined);
-    expect(result.length).toEqual(2);
-  });
-});
+    })
+    expect(result[2]).toBe(undefined)
+    expect(result.length).toEqual(2)
+  })
+})
 
 describe('mostActive method', () => {
   test('should list the most active traders', () => {
     const customers = [
       'Omega', 'Alpha', 'Alpha',
-    ];
-    const result = mostActive(customers);
-    expect(result[0]).toBe('Alpha');
-    expect(result[1]).toBe('Omega');
-    expect(result[2]).toBe(undefined);
-  });
+    ]
+    const result = mostActive(customers)
+    expect(result[0]).toBe('Alpha')
+    expect(result[1]).toBe('Omega')
+    expect(result[2]).toBe(undefined)
+  })
   test(`should list the most active only
     if trader has at least 5% of total trades
     `, () => {
@@ -131,8 +131,8 @@ describe('mostActive method', () => {
       'Alpha',
       'Omega',
       'Beta',
-    ];
-    const result = mostActive(customers);
-    expect(result[2]).toBe('Omega');
-  });
-});
+    ]
+    const result = mostActive(customers)
+    expect(result[2]).toBe('Omega')
+  })
+})
