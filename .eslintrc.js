fe321da68e95d4813169a117a51ae9f60fbd6051
Aakash Shah <ashah@lowes.com>
2020-08-13 11:18:41 +0530

    Updated eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,14 @@
 module.exports = {
   parser: 'babel-eslint',
+  env: {
+    node: true,
+    jest: true,
+  },
   extends: [
     'google',
   ],
   rules: {
     'semi': [2, 'never'],
+    'no-undef': 2,
   },
 }
