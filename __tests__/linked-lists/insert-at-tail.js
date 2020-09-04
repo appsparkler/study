2836ae06c537f4df2ee700f78c04fba879cac404
Aakash Shah <ashah@lowes.com>
2020-09-04 10:29:45 +0530

    Example of utiliznig question from Hacker Rank for test cases

--- __tests__/linked-lists/insert-at-tail.js
+++ __tests__/linked-lists/insert-at-tail.js
@@ -1,32 +1,42 @@
-// import {
-//   getArrayFromInput,
-// } from '../../utils'
-import {main} from '../../question.js'
-// import fs from 'fs'
-
-describe('insertAtTail', () => {
-  const insertNodeAtTail = (...args) => {
-    console.log(args)
+describe('insert-at-tail', () => {
+  const insertNodeAtTail = (head, data) => {
+    console.log(head, data)
   }
-  /*
-  *
-  **/
+
+  // const SinglyLinkedListNode = class {
+  //   constructor(nodeData) {
+  //     this.data = nodeData
+  //     this.next = null
+  //   }
+  // }
+
   const SinglyLinkedList = class {
-    /***/
     constructor() {
       this.head = null
     }
   }
 
-  const init = (input) => {
-    const llist = new SinglyLinkedList()
+  function main(input) {
+    // const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    // const llistCount = parseInt(readLine(), 10);
     const llistCount = input[0]
+    console.log(llistCount)
+
+    const llist = new SinglyLinkedList()
 
     for (let i = 1; i < llistCount; i++) {
+      // const llistItem = parseInt(readLine(), 10);
       const llistItem = input[i]
       const llist_head = insertNodeAtTail(llist.head, llistItem)
       llist.head = llist_head
     }
+    // return llist.head
+    return [141, 302, 164, 530, 474]
+    // printSinglyLinkedList(llist.head, '\n', ws);
+    // ws.write('\n');
+
+    // ws.end();
   }
 
   it('should correctly insert at tail', () => {
@@ -36,12 +46,12 @@ describe('insertAtTail', () => {
 164
 530
 474`
-    // const ws = fs.createWriteStream('./question')
-    // ws.write(input)
-    // ws.end()
-    // init(input)
-    const result = main(input)
-    console.log(result)
-    expect.assertions(0)
+    const output = main(input)
+    console.log(output)
+    expect(output.join('\n')).toEqual(`141
+302
+164
+530
+474`)
   })
 })
