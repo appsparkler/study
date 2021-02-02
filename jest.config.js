2537fa341b80f6f48d3169b8dc107cbf9e8355e5
Aakash <subs@appsparkler.com>
2021-02-02 23:17:39 +0530

    Typescript setup

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  'setupFiles': [
-    '<rootDir>/jest.init.js',
-  ],
+  // 'setupFiles': [
+  //   '<rootDir>/jest.init.js',
+  // ],
+  'preset': 'ts-jest',
+  'transform': {
+    '^.+\\.(ts|tsx)?$': 'ts-jest',
+    '^.+\\.(js|jsx)$': 'babel-jest',
+  },
 }
