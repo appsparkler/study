3f9949b0c861e6579d4f84368eda9ccc0cb81a6d
Aakash Shah <subs@appsparkler.com>
2023-02-02 16:18:03 +0530

    chore: q&a game

--- src/App.java
+++ src/App.java
@@ -2,15 +2,15 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        String question = "What is 2 + 2?";
-        String firstChoice = "4";
+        String question = "Which is the largest planet in our solar system - Earth, Venus or Jupiter?";
+        String firstChoice = "Jupiter";
         String correctAnswer = firstChoice;
         System.out.println(question);
         boolean hasAnsweredCorrectly = false;
         while (!hasAnsweredCorrectly) {
             Scanner userAnswerScanner = new Scanner(System.in);
             String userAnswer = userAnswerScanner.next();
-            if (userAnswer.equals(correctAnswer)) {
+            if (userAnswer.toLowerCase().equals(correctAnswer.toLowerCase())) {
                 hasAnsweredCorrectly = true;
                 System.out.println("That's the right answer!! Congrats!!!");
                 userAnswerScanner.close();
