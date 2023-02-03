d5a3e5689f6ae25b67044314c4c91be4656a90e2
Aakash Shah <subs@appsparkler.com>
2023-02-03 12:09:50 +0530

    chore: fix errors in code with debugger

--- src/App.java
+++ src/App.java
@@ -1,22 +1,31 @@
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        String question = "Which is the largest planet in our solar system - Earth, Venus or Jupiter?";
-        String firstChoice = "Jupiter";
-        String correctAnswer = firstChoice;
-        System.out.println(question);
-        boolean hasAnsweredCorrectly = false;
-        while (!hasAnsweredCorrectly) {
-            Scanner userAnswerScanner = new Scanner(System.in);
-            String userAnswer = userAnswerScanner.next();
-            if (userAnswer.toLowerCase().equals(correctAnswer.toLowerCase())) {
-                hasAnsweredCorrectly = true;
-                System.out.println("That's the right answer!! Congrats!!!");
-                userAnswerScanner.close();
-            } else {
-                System.out.println("Please try again...");
-            }
+
+    public static void main(String[] args) {
+
+        System.out.println("Let's calculate the area of a triangle");
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please input the base of the triangle (in inches).");
+        double base = input.nextDouble();
+
+        while (base <= 0) {
+            System.out.println("That's invalid. Please input the base of the triangle (in inches).");
+            base = input.nextDouble();
         }
+
+        System.out.println("Please input the height of the triangle (in inches).");
+        double height = input.nextDouble();
+
+        while (height <= 0) {
+            System.out.println("That's invalid. Please input the height of the triangle (in inches).");
+            height = input.nextDouble();
+        }
+
+        double area = (base * height) / 2;
+        System.out.println("The area is " + area);
+        input.close();
     }
 }
