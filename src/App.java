444258a4b63f6982f3242ba027b3ed86f8aaa9e1
Aakash Shah <subs@appsparkler.com>
2023-02-03 13:38:27 +0530

    chore: clean up

--- src/App.java
+++ src/App.java
@@ -1,25 +1,7 @@
 import java.util.Scanner;
 
 public class App {
-
-    public static void announceDeveloperTeaTime(Scanner scanner) {
-        System.out.println("Type in a random word to announce developer tea time");
-        scanner.next();
-        System.out.println("Its developer tea time!!! 🫖");
-    }
-
     public static void main(String[] args) {
-        System.out.println("Welcome to your new job!!");
-        Scanner scanner = new Scanner(System.in);
-        
-        announceDeveloperTeaTime(scanner);
-
-        System.out.println("Write Code");
-        System.out.println("have meetings");
-        System.out.println("Review code");
-
-        announceDeveloperTeaTime(scanner);
-
-        System.out.println("Yay!! You got promoted!!");
+       
     }
 }
