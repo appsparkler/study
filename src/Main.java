06f04f877fd5f67b2f122df5e7679db02b263eb4
Aakash Shah <subs@appsparkler.com>
2023-02-12 16:22:40 +0530

    chore: substitution ...

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         build(office);
     }
 
-    private static void build(Building building) {
+    private static void build(Building building) { // substitution - any sub-type of Building can be passed to this method
         System.out.println("Constructing a new " + building.toString());
     }
 }
