4ea87276b4459eb1689b00c5881aee5eee328888
Aakash Shah <subs@appsparkler.com>
2023-02-12 15:02:01 +0530

    chore: generic methods

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    
+    static String[] strArray = {};
+    static Integer[] intArray = {1,2,3};
+    static Boolean[] boolArray = {true, false, true};
+
+    public static <T>List<T> arrayToList(T[] arr) {
+        List<T> list = new ArrayList<T>();
+        for (T obj : arr) {
+            list.add(obj);
+        }
+        return list;
+    }
+
+    public static void main(String[] args)
+    {
+        List<String> strList = arrayToList(strArray);
+        List<Integer> intList = arrayToList(intArray);
+        List<Boolean> boolList = arrayToList(boolArray);
+
+        strList.add("Yo!");
+
+        System.out.println(strList.get(0));
+        System.out.println(intList.get(0));
+        System.out.println(boolList.get(0));
+    }
 }
