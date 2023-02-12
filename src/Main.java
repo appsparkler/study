2fad30262fe1f98ff39c6b69b60fb72923dda126
Aakash Shah <subs@appsparkler.com>
2023-02-12 16:17:02 +0530

    chore: clean up

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,3 @@
 public class Main {
-    public static void printShoppingList(String... items) {
-        System.out.println("\n");
-        System.out.println("SHOPPING LIST");
-        for (int i = 0; i < items.length; i++) {
-            System.out.println((i + 1) + ". " + items[i]);
-        }
-    }
-
-    public static void main(String... args) {
-        printShoppingList("Orange", "Banana", "Apple");
-        printShoppingList("Hybrid Tomato");
-    }
+    
 }
