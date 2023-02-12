0c4d91cd31a033cf081392e8340a6786df7d97df
Aakash Shah <subs@appsparkler.com>
2023-02-12 16:21:58 +0530

    chore: an example of substituion in Java

---
+++ src/Building.java
@@ -0,0 +1,6 @@
+public class Building {
+    @Override 
+    public String toString() {
+        return "building";
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,13 @@
 public class Main {
-    
+    public static void main(String... args) {
+        Building building = new Building();
+        Office office = new Office();
+
+        build(building);
+        build(office);
+    }
+
+    private static void build(Building building) {
+        System.out.println("Constructing a new " + building.toString());
+    }
 }
