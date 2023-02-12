7b964b7b5254eb88718eb4c1840d9147ce7e21f9
Aakash Shah <subs@appsparkler.com>
2023-02-12 18:29:44 +0530

    chore: generics super and extends

---
+++ src/House.java
@@ -0,0 +1,7 @@
+public class House extends Building {
+    @Override
+    public String toString() {
+        return "house";
+    }
+    
+}
--- src/Main.java
+++ src/Main.java
@@ -6,19 +6,29 @@ public class Main {
         List<Building> buildingList = new ArrayList<>();
         buildingList.add(new Building());
         buildingList.add(new Office());
-
         printBuildings(buildingList);
+        addHouseToList(buildingList);
 
         List<Office> officeList = new ArrayList<>();
         officeList.add(new Office());
         officeList.add(new Office());
-
         printBuildings(officeList);
+
+        List<House> houseList = new ArrayList<>();
+        houseList.add(new House());
+        houseList.add(new House());
+        printBuildings(houseList);
+        addHouseToList(houseList);
+
     }
 
     private static void printBuildings(List<? extends Building> buildingList) {
         for (int i = 0; i < buildingList.size(); i++) {
             System.out.println("Building " + buildingList.get(i));
         }
+    }
+
+    private static void addHouseToList(List<? super House> buildings) {
+        buildings.add(new House());
     }
 }
