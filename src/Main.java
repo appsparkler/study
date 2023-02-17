4d5336f1758f5eec87779c4e06dfadece2b2afc6
Aakash Shah <subs@appsparkler.com>
2023-02-17 06:24:20 +0530

    chore: LinkedHashMaps

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        
+        LinkedHashMap<String, Integer> lhm = new LinkedHashMap<String, Integer>();
+        lhm.put("Brenda", 38383883);
+        lhm.put("John", 3111323);
+        lhm.put("Aakash", 73380);
+        lhm.put("Mohammed", 2232333);
+
+        for(Map.Entry<String,Integer> entry: lhm.entrySet()) {
+            System.out.println(entry.getKey());
+        }
+
+
+        // System.out.println(lhm);
     }
 }
