4000385412c788b71de88d5a2c689a1cbb276f55
Aakash Shah <subs@appsparkler.com>
2023-02-17 06:02:08 +0530

    chore: clean up

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,6 @@ import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-        HashMap phonebook = new HashMap<String, Integer>();
-        phonebook.put("Aakash", "7338080855");
-        phonebook.put("Brenda", 83838383);
-        phonebook.put(null, 00000);
-        phonebook.put("Smita", 383823);
-        System.out.println(phonebook);
-        phonebook.put("Smita", 381111);
-        if (phonebook.containsKey("Smita")) {
-            phonebook.remove("Smita");
-        }
-        System.out.println(phonebook);
-        phonebook.clear();
-        System.out.println(phonebook);
+        
     }
 }
