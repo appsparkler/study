671a4dbb81016f717a8a874e70fdda0a77d5a26a
Aakash Shah <subs@appsparkler.com>
2023-02-17 07:39:53 +0530

    chore: functional interface

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,11 @@
-import java.util.LinkedHashMap;
-import java.util.Map;
-
 public class Main {
     public static void main(String[] args) {
-        LinkedHashMap<String, Integer> lhm = new LinkedHashMap<String, Integer>();
-        lhm.put("Brenda", 38383883);
-        lhm.put("John", 3111323);
-        lhm.put("Aakash", 73380);
-        lhm.put("Mohammed", 2232333);
-
-        for(Map.Entry<String,Integer> entry: lhm.entrySet()) {
-            System.out.println(entry.getKey());
-        }
+        GreetingMessage gm = new GreetingMessage() {
+            @Override
+            public void greet(String name) {
+                System.out.println("Hello, " + name + "!🙂");
+            }
+        };
+        gm.greet("Aakash");
     }
 }
