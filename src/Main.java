cef46c14b2a18441b3395a5eea6b71f9a8092492
Aakash Shah <subs@appsparkler.com>
2023-02-25 15:33:48 +0530

    chore: breaking the deadlock

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,9 @@ public class Main {
     public static Object bowl = new Object();
     public static void main(String[] args) {
        Thread cook1 = new Thread(() -> {
-        synchronized(spoon) {
-            System.out.println("Cook 1: Holding the spoon");
-            System.out.println("Cook 2: Waiting for the bowl");
+        synchronized(bowl) {
+            System.out.println("Cook 1: Holding the bowl");
+            System.out.println("Cook 2: Waiting for the spoon");
 
             synchronized(bowl) {
                 System.out.println("Cook 1: Holding the spoon & the bowl.");
