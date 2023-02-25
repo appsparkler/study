67db357322eaeb251e388e6c66a5ebddec4ae367
Aakash Shah <subs@appsparkler.com>
2023-02-25 09:09:53 +0530

    chore: clean up

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        // With the thread logic abstracted out in Runny
-        Thread t1 = new Thread(new Runny());
-        t1.start();
-
-        // With Runnable defined within the main function itself
-        Thread t2 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < 100; i++) {
-                    System.out.println(Thread.currentThread().getName() + "-" + i);
-                }
-            }
-        });
-        t2.start();
-
-        // With lambda (implementing the Runnable @Functional Interface)
-        Thread t3 = new Thread(() -> {
-                for (int i = 0; i < 100; i++) {
-                    System.out.println(Thread.currentThread().getName() + "-" + i);
-                }
-        });
-        t3.start();
+        
     }
 }
