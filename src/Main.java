06d82ee1484db31472cd52581d1bd8897edd3a39
Aakash Shah <subs@appsparkler.com>
2023-02-25 06:28:00 +0530

    chore: clean up

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Thready t1 = new Thready();
-        t1.setName("T1");
-        t1.start();
-
-        Thready t2 = new Thready();
-        t2.setName("T2");
-        t2.start();
-
-        System.out.println(Thread.activeCount());
+        
     }
     
 }
--- src/Thready.java
+++
@@ -1,8 +0,0 @@
-public class Thready extends Thread {
-    @Override
-    public void run() {
-        for (int i = 0; i < 100; i++) {
-            System.out.println(i + " " +this.getName());
-        }
-    }
-}
