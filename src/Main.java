0417aaf5f36070d1f618e3e77d7f84ed117e8848
Aakash Shah <subs@appsparkler.com>
2023-02-25 15:33:29 +0530

    chore: threads in deadlock

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
+    public static Object spoon = new Object();
+    public static Object bowl = new Object();
     public static void main(String[] args) {
-        
+       Thread cook1 = new Thread(() -> {
+        synchronized(spoon) {
+            System.out.println("Cook 1: Holding the spoon");
+            System.out.println("Cook 2: Waiting for the bowl");
+
+            synchronized(bowl) {
+                System.out.println("Cook 1: Holding the spoon & the bowl.");
+            }
+        }
+       });
+
+       Thread cook2 = new Thread(() -> {
+        synchronized(bowl) {
+            System.out.println("Cook 1: Holding the bowl");
+            System.out.println("Cook 2: Waiting for the spoon");
+
+            synchronized(spoon) {
+                System.out.println("Cook 1: Holding the bowl & the spoon.");
+            }
+        }
+       });
+
+       cook1.start();
+       cook2.start();
     }
 }
