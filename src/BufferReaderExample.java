7a8f6a6e0b69bf4adb60a11d4cb6dc6bb4c3ddb6
Aakash Shah <subs@appsparkler.com>
2023-03-03 09:53:49 +0530

    chore: using "try" with resources to ensure that they are properly closed

--- src/BufferReaderExample.java
+++ src/BufferReaderExample.java
@@ -2,17 +2,20 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.StringWriter;
 
 public class BufferReaderExample {
     public static void main(String[] args) {
         File myFile = new File("example.txt");
-        try(
+        try (
             BufferedReader reader = new BufferedReader(new FileReader(myFile));
+            StringWriter writer = new StringWriter();
         ) {
             String line;
             while ((line = reader.readLine()) != null) {
-                System.out.println(line);
+                writer.write(line);
             }
+            System.out.println(writer.toString());
         } catch (IOException e) {
             
         }
