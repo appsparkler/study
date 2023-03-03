7658b75b29221cb465baaee6969cf687eee261ee
Aakash Shah <subs@appsparkler.com>
2023-03-03 09:47:55 +0530

    chore: use try with resources to avoid resource leaks

--- src/BufferReaderExample.java
+++ src/BufferReaderExample.java
@@ -6,13 +6,13 @@ import java.io.IOException;
 public class BufferReaderExample {
     public static void main(String[] args) {
         File myFile = new File("example.txt");
-        try {
+        try(
             BufferedReader reader = new BufferedReader(new FileReader(myFile));
+        ) {
             String line;
             while ((line = reader.readLine()) != null) {
                 System.out.println(line);
             }
-            reader.close();
         } catch (IOException e) {
             
         }
